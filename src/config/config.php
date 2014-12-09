b6fe2b9594fb97db0503abfdc5502943083e9b62
Ben Allfree <ben@benallfree.com>
2014-12-09 08:55:04 -0800

    Updated config sample

--- src/config/config.php
+++ src/config/config.php
@@ -1,13 +1,27 @@
 <?php
-  
+use Imagine\Image\ImageInterface;
+
 /* Specify an array of sizes to be maintained */
+$sizes = [
+  'large' => '640x640#',
+  'featured' => '585x585#',
+  'medium' => '400x400#',
+  'thumb' => '180x180#',
+  'admin' => '100x100#',
+  'tiny' => '75x75#',
+];
+  
+foreach($sizes as $k=>$v)
+{
+  $sizes[$k] = [
+    'dimensions'=>$v,
+    'convert_options' => [
+      'jpeg_quality' => 90,
+      'resampling-filter' => ImageInterface::FILTER_CATROM,
+    ],
+  ];
+}
+
 return [
-  'sizes'=>[
-    'large' => '640x640#',
-    'featured' => '585x585#',
-    'medium' => '400x400#',
-    'thumb' => '180x180#',
-    'admin' => '100x100#',
-    'tiny' => '75x75#',
-  ],
+  'sizes'=>$sizes,
 ];
